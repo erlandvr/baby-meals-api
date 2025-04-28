ef82b10c3da93a9521c7c1b06db7298727ce2b9d
erlandvr <erlandvr@gmail.com>
2025-04-28 23:09:14 +0200

    Implement Create Child (POST /v1/children)
    next step: deploy and test agains real AWS

---
+++ baby-meals-api/src/children/get.py
@@ -0,0 +1,28 @@
+import os, json, uuid, datetime as dt
+import boto3
+
+# TODO Create GET children call
+""" # Initialize the table client once per container
+TABLE = boto3.resource("dynamodb").Table(os.environ["TABLE_NAME"])
+
+def lambda_handler(event, context):
+    body = json.loads(event.get("body", "{}"))
+    # Basic validation
+    if not body.get("name") or not body.get("dateOfBirth"):
+        return {"statusCode": 400, "body": json.dumps({"message":"name & dateOfBirth required"})}
+
+    child_id = str(uuid.uuid4())
+    item = {
+        "id": child_id,
+        "name": body["name"],
+        "dateOfBirth": body["dateOfBirth"],
+        "createdAt": dt.datetime.utcnow().isoformat()
+    }
+    # **persist** to DynamoDB
+    TABLE.put_item(Item=item)
+
+    return {
+        "statusCode": 201,
+        "body": json.dumps(item)
+    }
+ """
